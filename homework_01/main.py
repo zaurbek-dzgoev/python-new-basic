c65891763ddca950c7a3c67e37277429620fc320
zaurbek-dzgoev <119847363+zaurbek-dzgoev@users.noreply.github.com>
2023-01-16 23:50:48 +0300

    Add homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -35,25 +35,24 @@ def is_prime(x):
             n = 1
     return(n)
 
-def filter_numbers():
+def filter_numbers(number_list, filter_type):
     if filter_type == ODD:
         print([number for number in number_list if number % 2 != 0])
     if filter_type == EVEN:
         print([number for number in number_list if number % 2 == 0])
     if filter_type == PRIME:
-        is_prime()
-        for item in number_list:
-            if item in is_prime() == 1:
-                print(item)
+        print([number for number in number_list if is_prime(number) == 1])
 
-# filter_numbers([1, 2, 24, 45], PRIME)
+filter_numbers([1, 2, 3], ODD)
+filter_numbers([2, 3, 4, 5], EVEN)
+filter_numbers([1, 2, 24, 45], PRIME)
     # """
     # функция, которая на вход принимает список из целых чисел,
     # и возвращает только чётные/нечётные/простые числа
     # (выбор производится передачей дополнительного аргумента)
     #
-    filter_numbers([1, 2, 3], ODD)
+
     # <<< [1, 3]
-    filter_numbers([2, 3, 4, 5], EVEN)
+    #
     # <<< [2, 4]
     # """
